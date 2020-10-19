1a23566cdf5184aca0261d05e3b48cc9b1ebba79
Maxim Bozhko <guardian.gmain@gmail.com>
2020-10-19 21:08:19 +0300

    optimised version

--- lab_km_1/client.py
+++ lab_km_1/client.py
@@ -1,25 +1,31 @@
 import socket
-import json
 
 HOST = '127.0.0.1'
 PORT = 65432
 command_set = ["Who", "Start game", "Exit"]
 
-def receive_data(inp):
-    inp = inp.replace("\'", "\"")
-    data = json.loads(inp)
-    return data
+def print_table(data):
+    table = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k"]
+    for s in data:
+        s = format(s, "08b")
+        for i in range(4):
+            table.append(int(s[2*i:2*i+2], 2))
+    for j in range(11):
+        print(str(j)+" "*(2-len(str(j))), end = " ")
+        for k in range(10):
+            print(table[10*j+k], end=" ")
+        print("")
 
-def print_data(data, closed):
-    for key in data:
-        if key == "text":
-            print (data[key])
-        elif key == "table":
-            for line in data[key]:
-                print(line)
-        elif key == "exit":
-            print(data[key])
-            closed = True
+def print_text(data):
+    print(data.decode("utf-8"))
+
+def print_data(head, data):
+    if head == 1: #text
+        print_text(data)
+    elif head == 2: #table
+        print_table(data)
+    elif head == 3: #stat
+        pass
     return closed
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -30,12 +36,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         command = str(input("Enter command:\n"))
         if command in command_set or command[0:6] == "Shoot ":
             s.send(command.encode("utf-8"))
-            data = receive_data(s.recv(1024).decode("utf-8"))
-            closed = print_data(data, closed)
+            data = s.recv(1024)
+            if data[0]:
+                print_data(data[0], data[1:])
+            else:
+                print(data[1:].decode("utf-8"))
+                closed = True
     s.close()
-            
-                    
-                    
-                    
-            
-            
--- lab_km_1/server.py
+++ lab_km_1/server.py
@@ -6,17 +6,23 @@ HOST = '127.0.0.1'
 PORT = 65432
 
 def form_table():
-    head = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "k"]
-    table = [[("0" if i else str(j)) for i in range(11)] if j else head for j in range(11)]
+    table = bytes([0 for i in range(25)])
     return table
-
+    
 def form_stat():
     stat = [0, 0, 0]
     return stat
 
 def change_table(table, i, j):
-    if table[i][j]!="X":
-        table[i][j] = "X"
+    byte_pos = (10*i+j)//4
+    from_pos = 2*((10*i+j)%4)
+    to_pos = 2*((10*i+j)%4+1)
+    bt = format(table[byte_pos], "08b")
+    bit2 = int(bt[from_pos:to_pos], 4)
+    if not bit2:
+        bit2 = format(int("1", 4), "02b")
+        bt = bt[:from_pos]+bit2+bt[to_pos:]
+        table = table[:byte_pos]+bytes([int(bt, 2)])+table[byte_pos:]
         return table
     else:
         return "Already"
@@ -28,75 +34,68 @@ def check_tail(inp):
         c1 = int(inp[:len(inp)-1])
         c2 = inp[len(inp)-1]
         if c1 in coords1  and c2 in coords2:
-            return c1, coords2.index(c2)+1
+            return c1-1, coords2.index(c2)
         else:
             return "Error"
     except:
         return "Error"
 
-def Who():
-    output = {}
-    text = "Божко Максим, К-23"
-    output["text"] = text
-    conn.send(bytes(str(output), "utf-8"))
+def Who(conn):
+    output= bytes([1]) + bytes("Божко Максим, К-23", "utf-8")
+    conn.send(output)
 
-def Start_game():
-    output = {}
+def Start_game(conn):
     table = form_table()
     stat = form_stat()
-    output["table"] = table
-    conn.send(bytes(str(output), "utf-8"))
+    output = bytes([2]) + table
+    conn.send(output)
+    started = True
     return table, stat
 
-def Shoot(data, table):
-    output = {}
+def Shoot(conn, data, table):
     numbers = check_tail(data)
     if numbers=="Error":
-        output["text"] = "incorrect coords. Example: shoot 5b"
-        conn.send(bytes(str(output), "utf-8"))
+        output = bytes([1]) + bytes("incorrect coords. Example: shoot 5b", "utf-8")
+        conn.send(output)
     else:
         ans = change_table(table, numbers[0], numbers[1])
         if ans=="Already":
-            output["text"] = "Already attacked"
-            conn.send(bytes(str(output), "utf-8"))
+            output = bytes([1]) + bytes("Already attacked", "utf-8")
+            conn.send(output)
         else:
             table = ans
-            output["table"] = table
-            conn.send(bytes(str(output), "utf-8"))
+            output = bytes([2]) + table
+            conn.send(output)
     return table
 
-def Exit():
-    output = {}
-    text = "Connection closed"
-    output["exit"] = text
-    conn.send(bytes(str(output), "utf-8"))
+def Exit(conn):
+    output = bytes([0]) + bytes("Connection closed", "utf-8")
+    conn.send(output)
     conn.close()
 
 def Main(conn, addr):
     print("MAIN")
     table = None
+    started = False
     while True:
         print(addr)
         data = conn.recv(1024).decode("utf-8")
-        output = {}
-
         if data == "Who":
-            Who()
+            Who(conn)
         elif data == "Start game":
-            table, stat = Start_game()
+            if not_started:
+                table, stat = Start_game(conn)
+            else:
+                conn.send(bytes([1]) + bytes("Already started", "utf-8"))
         elif data[:5] == "Shoot":
-            table = Shoot(data[6:], table)
+            table = Shoot(conn, data[6:], table)
         elif data == "Exit":
-            Exit()
+            Exit(conn)
             break
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-
     while True:
         conn, addr = s.accept()
         start_new_thread(Main, (conn, addr))
-
-
-
